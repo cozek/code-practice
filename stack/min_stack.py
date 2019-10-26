b5058cefe6767a47c893aac932028a68a5632ca7
cozek <kaushikamardas@gmail.com>
2019-10-26 14:09:55 +0530

    adding minstack

--- stack/min_stack.py
+++ stack/min_stack.py
@@ -4,13 +4,14 @@ import stack
 This script implements MinStack which allows one to retrieve the
 min of the stack in O(1) and uses less space.
 """
+
+
 class MinStack(stack.MyStack):
     def __init__(self):
         super().__init__()
         self.__min_top = None
 
-
-    def push(self,item):
+    def push(self, item):
         super().push(item)
         if self.__min_top == None:
             self.__min_top = stack.StackNode(item)
@@ -25,13 +26,12 @@ class MinStack(stack.MyStack):
         if popped_item == self.get_min():
             self.__min_top = self.__min_top.next_node
 
-
     def get_min(self):
         return self.__min_top.data
 
 
 def main():
-    arr = [5,3,2,10,100]
+    arr = [5, 3, 2, 10, 100]
     stk = MinStack()
     [stk.push(num) for num in arr]
     print(stk)
@@ -41,5 +41,6 @@ def main():
         print(stk.get_min())
     print(stk.get_min())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
